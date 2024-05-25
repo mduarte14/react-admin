81359a5124dfb830ee71cd0dd77ba29c0e061823
Miguel Duarte <MDuarte@pavementrecycling.com>
2024-05-25 16:53:00 -0700

    TICKET-0000: Test commit merge. * Added a test commit to test if PR's are working.

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -5,7 +5,7 @@ const Dashboard = () => {
   return (
     <Box m="20px">
       <Box display="flex" justifyContent="space-between" alignItems="center">
-        <Header title="DASHBOARD" subtitle="Welcome to your dashboard" />
+        <Header title="DASHBOARD" subtitle="Welcome to your dashboard." />
       </Box>
     </Box>
   );
