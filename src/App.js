f6a5bf63118d215ed772a611c417f9417bb80b8c
Miguel Duarte <duartemiguel04@gmail.com>
2024-06-06 13:56:17 -0700

    *updated readme to remove placeholders * Added team scene * Added contacts scene

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ import { CssBaseline, ThemeProvider } from "@mui/material";
 import Topbar from "./scenes/global/Topbar";
 import SideBar from "./scenes/global/Sidebar";
 import Dashboard from "./scenes/dashboard";
-// import Team from "./scenes/Team";
+import Team from "./scenes/team";
+import Contacts from "./scenes/contacts";
 // import Invoices from "./scenes/Invoice";
-// import Contacts from "./scenes/contacts";
+
 // import Bar from "./scenes/bar";
 // import Form from "./scenes/form";
 // import Line from "./scenes/line";
@@ -27,8 +28,8 @@ function App() {
             <Topbar />
             <Routes>
               <Route path="/" element={<Dashboard />} />
-              {/* <Route path="/team" element={<Team />} /> */}
-              {/* <Route path="/contacts" element={<Contacts />} /> */}
+              <Route path="/team" element={<Team />} />
+              <Route path="/contacts" element={<Contacts />} />
               {/* <Route path="/invoices" element={<Invoices />} /> */}
               {/* <Route path="/form" element={<Form />} /> */}
               {/* <Route path="/bar" element={<Bar />} /> */}
